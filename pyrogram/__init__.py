474e28c87913e581a5541c952931f605e8b1b551
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2020-01-31 20:25:02 +0530

    Update __init__.py

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -16,9 +16,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.16.0.asyncio"
+__version__ = "2.0.2020"
 __license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-__copyright__ = "Copyright (C) 2017-2019 Dan <https://github.com/delivrance>"
+__copyright__ = "Copyright (C) 2020 KeralaPyApi"
 
 from .client import *
 from .client.handlers import *
